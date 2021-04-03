152c5489819b090dd67d642316d3af6473dc9989
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-03 17:06:52 +0000

    change widgets to todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,21 +1,21 @@
 /*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
+ * All routes for todos are defined here
+ * Since this file is loaded in server.js into api/todos,
+ *   these routes are mounted onto /todos
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
+    let query = `SELECT * FROM todos`;
     console.log(query);
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const todos = data.rows;
+        res.json({ todos });
       })
       .catch(err => {
         res
