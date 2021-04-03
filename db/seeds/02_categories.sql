4ce9fd6d5ce8f757eca81260fdaf0eb928e16ce8
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-03 20:19:37 +0000

    Normalize database by adding categories table

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,9 @@
+-- Drop and recreate categories table
+
+INSERT INTO categories (
+name
+) VALUES ('watch'),
+('read'),
+('eat'),
+('buy'),
+('miscellaneous');
---
+++ db/seeds/03_todos.sql
@@ -0,0 +1,37 @@
+-- Todos table seeds here
+INSERT INTO todos (
+  user_id,
+  category_id,
+  name,
+  note
+) VALUES (1, 5, 'Lord of the Rings', 'all three marathon!');
+
+INSERT INTO todos (
+ user_id,
+  category_id,
+  name,
+  note
+) VALUES (1, 4, 'ten thousand ladybugs', 'amazon?');
+
+INSERT INTO todos (
+  user_id,
+  category_id,
+  name
+) VALUES (1, 4, 'honey');
+
+INSERT INTO todos (
+  user_id,
+  category_id,
+  name
+) VALUES (1, 2, 'newspaper');
+
+INSERT INTO todos (
+  user_id,
+  category_id,
+  name
+) VALUES (1, 3, 'starbucks');
+
+INSERT INTO todos (
+  user_id,
+  name
+) VALUES (1, 'space');
