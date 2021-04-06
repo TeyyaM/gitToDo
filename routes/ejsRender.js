2714ecfb0d6cbe0b448fee57efd3556f17f2c922
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-04-06 22:39:40 +0000

    refactor

--- routes/ejsRender.js
+++ routes/ejsRender.js
@@ -1,23 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (pool) => {
-// Home page
+  // Home page
   router.get("/", (req, res) => {
     const templateVars = {
       user_id: req.session.user_id,
-      index: true
+      index: true,
     };
     res.render("index", templateVars);
   });
   router.get("/todos/categories", (req, res) => {
     const templateVars = {
       user_id: req.session.user_id,
-      index: true
+      index: true,
     };
     res.render("todo_categories", templateVars);
   });
-
 
   // category 1, 2, 3 etc...
   router.get("/todos/categories/:category_id", (req, res) => {
@@ -27,36 +26,35 @@ module.exports = (pool) => {
     FROM todos
     WHERE user_id = $1`;
     if (category_id !== "all") {
-      query +=  ` AND category_id = $2`;
+      query += ` AND category_id = $2`;
       queryParams.push(category_id);
     }
-    pool.query(query, queryParams)
-      .then(data => {
-        const templateVars = {
-          user_id: req.session.user_id,
-          index: false,
-          todos: data.rows
-        };
-        res.render("todo_list", templateVars);
-      })
+    pool.query(query, queryParams).then((data) => {
+      const templateVars = {
+        user_id: req.session.user_id,
+        index: false,
+        todos: data.rows,
+      };
+      res.render("todo_list", templateVars);
+    });
   });
-
 
   router.get("/todos/:todo_id", (req, res) => {
     const query = `SELECT *
     FROM todos
     WHERE user_id = $1 AND id = $2;`;
     const queryParams = [req.session.user_id, req.params.todo_id];
-    pool.query(query, queryParams)
-    .then(data => {
+    pool.query(query, queryParams).then((data) => {
       const templateVars = {
         user_id: req.session.user_id,
         index: false,
-        todos: data.rows[0]
+        todos: data.rows[0],
       };
       res.render("todo_show", templateVars);
     });
   });
+
+  router.post("todos/:todo_id/edit", (req, res) => {});
+
   return router;
 };
-
