f24142e40fdd08d70c173599ada923dda17c2be3
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-06 22:37:23 +0000

    Move POST for new todos into ejsRender.js

--- routes/ejsRender.js
+++ routes/ejsRender.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { newTodoQuery } = require("../public/scripts/helpers");
 
 module.exports = (pool) => {
-// Home page
+  // Home page
   router.get("/", (req, res) => {
     const templateVars = {
       user_id: req.session.user_id,
@@ -27,7 +28,7 @@ module.exports = (pool) => {
     FROM todos
     WHERE user_id = $1`;
     if (category_id !== "all") {
-      query +=  ` AND category_id = $2`;
+      query += ` AND category_id = $2`;
       queryParams.push(category_id);
     }
     pool.query(query, queryParams)
@@ -54,6 +55,37 @@ module.exports = (pool) => {
       todos: data.rows
     };
     res.render("todo_show", templateVars);
+  });
+
+  router.post("/todos/new", (req, res) => {
+
+    const inputObj = {
+      user_id: req.session.user_id,
+      todoInput: req.body.todo,
+      // Parameters not required for a todo to be INSERTED
+      optional: {
+        note: req.body.note,
+        deadline: req.body.deadline
+      }
+    }
+    if (!inputObj.todoInput) {
+      // Important! Add error message later
+      console.log('Empty Todo!');
+    } else {
+      newTodoQuery(inputObj)
+        .then(returnObj => {
+          pool.query(returnObj.str, returnObj.arr)
+            .then(data => {
+              // get user_id and the todo's id from data RETURNING data
+              res.redirect(`/api/todos/${data.rows[0].user_id}/${data.rows[0].id}`);
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+        })
+    }
   });
 
   return router;
