160bbda986b112dbbe733f429262d1cf644016c8
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-07 03:27:58 +0000

    Progress on editing todos function in backend

--- routes/ejsRender.js
+++ routes/ejsRender.js
@@ -89,21 +89,32 @@ module.exports = (pool) => {
     }
   });
 
-  // // Complete or Delete a todo
 
   router.post("/todos/:todo_id/:column_name", (req, res) => {
+    // console.log(req);
+    // // Complete or Delete a todo
+    const queryParams = [req.session.user_id, req.params.todo_id];
     const column_name = req.params.column_name;
     let queryString = '';
     if (column_name === 'delete') {
       queryString = `DELETE
       FROM todos`;
     }
-    if ((column_name === 'complete')) {
+    if (column_name === 'complete') {
       queryString = `UPDATE todos
       SET date_completed = NOW()`;
     }
+    // WET, DRY later!!
+    // needs button to POST /todos/:todo_id/category
+    if (column_name === 'category_id' || column_name === 'note' || column_name === 'name') {
+      const attribute = '5' //change later to get from submit
+      queryString = `UPDATE todos
+      SET ${column_name} = ${attribute}`;
+    }
+
     queryString += ` WHERE user_id = $1 AND id = $2`;
-    const queryParams = [req.session.user_id, req.params.todo_id];
+    console.log(queryString)
+    console.log(queryParams)
     pool.query(queryString, queryParams)
       .then(() => {
         res.redirect("/todos/categories");
