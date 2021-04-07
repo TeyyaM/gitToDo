33c2c1146ea4ba12ea509a1838f70f4e3aa152ab
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-07 20:09:41 +0000

    Delete duplicate route, redirect POST to todos/new to redirect to the newly formed todo's page '

--- routes/ejsRender.js
+++ routes/ejsRender.js
@@ -117,7 +117,7 @@ module.exports = (pool) => {
           .then((data) => {
             // get user_id and the todo's id from data RETURNING data
             res.redirect(
-              `/api/todos/${data.rows[0].user_id}/${data.rows[0].id}`
+              `/todos/${data.rows[0].id}`
             );
           })
           .catch((err) => {
@@ -127,41 +127,5 @@ module.exports = (pool) => {
     }
   });
 
-
-  router.post("/todos/:todo_id/:column_name", (req, res) => {
-    // console.log(req);
-    // // Complete or Delete a todo
-    const queryParams = [req.session.user_id, req.params.todo_id];
-    const column_name = req.params.column_name;
-    let queryString = '';
-    if (column_name === 'delete') {
-      queryString = `DELETE
-      FROM todos`;
-    }
-    if (column_name === 'complete') {
-      queryString = `UPDATE todos
-      SET date_completed = NOW()`;
-    }
-    // WET, DRY later!!
-    // needs button to POST /todos/:todo_id/category
-    if (column_name === 'category_id' || column_name === 'note' || column_name === 'name') {
-      const attribute = '5' //change later to get from submit
-      queryString = `UPDATE todos
-      SET ${column_name} = ${attribute}`;
-    }
-
-    queryString += ` WHERE user_id = $1 AND id = $2`;
-    console.log(queryString)
-    console.log(queryParams)
-    pool.query(queryString, queryParams)
-      .then(() => {
-        res.redirect("/todos/categories");
-      })
-      .catch(err => {
-        res.status(500)
-          .json({ error: err.message });
-      });
-
-  });
   return router;
 };
