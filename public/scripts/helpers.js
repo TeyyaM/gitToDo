e8310f8e65ee1c22840a2f14f164bb469dd0e6c1
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-08 20:57:58 +0000

    refactoring

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,4 +1,56 @@
-const { findCategory } = require("./category-finder");
+const {
+  checkYelp,
+  checkBooks,
+  checkIMDB
+} = require("./apis");
+const stringSimilarity = require("string-similarity");
+
+async function findCategory(input) {
+
+  const threshold = 0.8;
+  const string = input.toLowerCase();
+
+  const categories = {
+    eat: {
+      num: 2,
+      apiFunc: checkYelp
+    },
+    read: {
+      num: 3,
+      apiFunc: checkBooks
+    },
+    watch: {
+      num: 4,
+      apiFunc: checkIMDB
+    },
+  }
+
+  const buy = {
+    num: 5
+    // apiFunc: add in future when launched, no paid apis for proof of concepts
+  }
+
+  // will call apis and check similarities in order, if there is a perfect match, it returns
+  for (const key in categories) {
+    const keyObj = categories[key]
+    keyObj.name = await keyObj.apiFunc(string);
+    keyObj.similarity = stringSimilarity.compareTwoStrings(string, keyObj.name.toLowerCase());
+    if (keyObj.similarity === 1) {
+      return { category: keyObj.num, name: keyObj.name };
+    }
+  }
+
+  // if there is no perfect match, it returns the closest if greater than threshold
+  for (const key in categories) {
+    const keyObj = categories[key]
+    if (keyObj.similarity > threshold) {
+      return { category: keyObj.num, name: keyObj.name };
+    }
+  }
+
+  // otherwise default to misc category (currently buy instead, due to apis not being free)
+  return { category: buy.num, name: string };
+};
 
 const newTodoQuery = (inputObj) => {
 
@@ -25,5 +77,6 @@ const newTodoQuery = (inputObj) => {
   })
 
 };
+
 module.exports = { newTodoQuery }
 
--- routes/ejsRender.js
+++ routes/ejsRender.js
@@ -4,7 +4,9 @@ const { newTodoQuery } = require("../public/scripts/helpers");
 const generateTodoHelpers = require("../db/helpers/todohelpers");
 
 module.exports = (pool) => {
-  const { fetchTodoByCategory } = generateTodoHelpers(pool);
+  const {
+    fetchTodoByCategory,
+    fetchTodoByTodoId } = generateTodoHelpers(pool);
   // Home page
   router.get("/", (req, res) => {
     const user_id = req.session.user_id;
@@ -18,10 +20,10 @@ module.exports = (pool) => {
         };
         res.render("index", templateVars);
       })
-    });
+  });
 
-    router.get("/todos/categories", (req, res) => {
-      const templateVars = {
+  router.get("/todos/categories", (req, res) => {
+    const templateVars = {
       user_id: req.session.user_id,
       index: false
     };
@@ -31,43 +33,28 @@ module.exports = (pool) => {
   // category 1, 2, 3 etc...
   router.get("/todos/categories/:category_id", (req, res) => {
     const category_id = req.params.category_id;
-    // const queryParams = [req.session.user_id];
-    // let query = `SELECT todos.id, todos.name, deadline, categories.name as category_name
-    // FROM todos
-    // JOIN categories ON todos.category_id = categories.id
-    // WHERE user_id = $1 AND date_completed IS NULL`;
-    // if (category_id !== "all") {
-    //   query += ` AND category_id = $2`;
-    //   queryParams.push(category_id);
-    // }
-    // pool.query(query, queryParams)
     fetchTodoByCategory(category_id, req.session.user_id).then((data) => {
       const templateVars = {
         user_id: req.session.user_id,
         index: false,
         todos: data.rows,
       };
-    console.log("TODOS", data.rows);
-    res.render("todo_list", templateVars);
+      console.log("TODOS", data.rows);
+      res.render("todo_list", templateVars);
     });
   });
 
   router.get("/todos/:todo_id", (req, res) => {
-    const query = `SELECT todos.name, todos.deadline, todos.date_added,
-    todos.date_completed, todos.note, categories.name as category
-    FROM todos
-    JOIN categories ON categories.id = todos.category_id
-    WHERE todos.user_id = $1 AND todos.id = $2;`;
-    const queryParams = [req.session.user_id, req.params.todo_id];
-    pool.query(query, queryParams).then((data) => {
-      const templateVars = {
-        user_id: req.session.user_id,
-        todo_id: req.params.todo_id,
-        index: false,
-        todo: data.rows[0]
-      };
-      res.render("todo_show", templateVars);
-    });
+    fetchTodoByTodoId(req.params.todo_id, req.session.user_id)
+      .then((data) => {
+        const templateVars = {
+          user_id: req.session.user_id,
+          todo_id: req.params.todo_id,
+          index: false,
+          todo: data.rows[0]
+        };
+        res.render("todo_show", templateVars);
+      });
   });
 
   // edit to do item
@@ -80,22 +67,17 @@ module.exports = (pool) => {
     if (column_name === 'delete') {
       queryString = `DELETE
       FROM todos`;
-    }
-    if (column_name === 'complete') {
+    } else if (column_name === 'complete') {
       queryString = `UPDATE todos
       SET date_completed = NOW()`;
-    }
-    // WET, DRY later!!
-    // needs button to POST /todos/:todo_id/category_id
-    if (column_name === 'category_id' || column_name === 'note' || column_name === 'name') {
+    } else {
+      // could work for category_id, note, name, or deadline with front-end funtionality
       const attribute = req.body.dbIndex
       queryString = `UPDATE todos
       SET ${column_name} = ${attribute}`;
     }
 
     queryString += ` WHERE user_id = $1 AND id = $2`;
-    // console.log(queryString)
-    // console.log(queryParams)
     pool.query(queryString, queryParams)
       .then(() => {
         res.redirect("/todos/categories");
